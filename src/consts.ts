34b52cc30231f3cccf7363b2fa33d48b7c576d8f
K3vnDev <158790560+K3vnDev@users.noreply.github.com>
2024-11-02 00:15:59 -0500

    Fix missing subsets on custom font issue

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,8 @@ import { Poppins } from 'next/font/google'
 // Fonts
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
-  variable: '--ff-poppins'
+  variable: '--ff-poppins',
+  subsets: ['latin']
 })
 
 export const fonts = {
